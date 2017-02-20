a957fa495978b21a7637538fcba30184aa6dca97
Fernando Quevedo <fernandoquevedo@MacBook-Pro-de-Fernando.local>
2017-02-19 22:30:00 -0300

    11 commit

--- models/clients.js
+++ models/clients.js
@@ -5,7 +5,15 @@ const Schema = mongoose.Schema
 
 const ClientsSchema = Schema({
 	name: String,
-	email: String
+	email: String,
+	celphone: String,
+	amount_ppl: Number,
+	date_ini: Date,
+	date_fin: Date,
+	amount_per_day: Number,
+	amount_canceled: Number,
+	state: String,
+	room_type: String
 	//category: { type:String, enum:['pieza doble','pieza individual']}
 })
 /*
