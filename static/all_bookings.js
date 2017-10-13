c0bd4b4ede5841f616ca4208ec3a6a6ee70b1f87
Fernando Quevedo <fdo.quevedo.alonso@gmail.com>
2017-10-13 02:02:46 -0300

    Add files via upload

--- static/all_bookings.js
+++ static/all_bookings.js
@@ -1,33 +1,24 @@
 /*jslint node: true */
 /* global io: true */
 "use strict";
-
 var socket = io.connect('127.0.0.1:3001');
 
 
-socket.on('messages',(data) => {
-	// render table with booking information
+//escucha la conexion en el socket llamado all_bookings y renderea al recibir
+socket.on('all_bookings',(data) => {
+
 	render(data);
-});
-
-socket.on('message_to_client',(data) => {
-	// render table with booking information
-	$('.connected_users').append(data+"<br>");
-
-});
-
-socket.on('socketid',(data) => {
-	// render table with booking information
 	console.log(data);
+	
 
 });
+
 
 
 
 function render(data){
 
 	var table = document.getElementById('fq_table');
-
 	if (data.length){
 		table.innerHTML='<tr><th>Ingreso</th><th>Salida</th><th>Nombre</th><th>Email</th><th>Telefono</th><th>Personas</th><th>Cobro</th><th>Cancelado</th><th>Tipo</th><th>Estado</th></tr>';
 		for (var i=0;i<data.length;++i){
@@ -37,25 +28,10 @@ function render(data){
 			table.innerHTML+='<tr><td>'+date_i+'</td><td>'+date_f+'</td><td>'+data[i].name+'</td>'+'</td><td>'+data[i].email+'</td>'+'</td><td>'+data[i].celphone+'</td>'+'</td><td>'+data[i].amount_ppl+'</td>'+'</td><td>'+data[i].amount_per_day+'</td>'+'</td><td>'+data[i].amount_canceled+'</td>'+'</td><td>'+data[i].room_type+'</td>'+'</td><td>'+data[i].state+'</td></tr>';
 		}
 	}
-	else {
-
-		var input_date = format_date(document.getElementById('search').value);
-		//ECMAScript 6
-		table.innerHTML=`<br/><br/><h6 align=center>No se han encontrado reservas para la fecha ${input_date}</h6>`;
-	}
-}
-
-function addMessage(){
-
-	var msg = document.getElementById('search').value;
-	var date_time = new Date(msg).toISOString();
-	//emit input date to new-message socket
-  	socket.emit('new-message', date_time);
-	return false;
 }
 
 function format_date(date){
-	
+
 	var input_date = new Date(date);
 	var dd =  input_date.getUTCDate();
 	var mm = input_date.getUTCMonth()+1;
@@ -71,3 +47,4 @@ function leadingZero(number) {
   }
   return number.toString();
 }
+
